7bb1fde5f0146289e9774a423d34bdae8247811c
Mariem Neili <mariemneili7@gmail.com>
2022-07-12 09:08:52 +0100

    init project

---
+++ src/main/java/syre/ms/microserviceproduct/MicroServiceProductApplication.java
@@ -0,0 +1,13 @@
+package syre.ms.microserviceproduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServiceProductApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroServiceProductApplication.class, args);
+    }
+
+}
---
+++ src/test/java/syre/ms/microserviceproduct/MicroServiceProductApplicationTests.java
@@ -0,0 +1,13 @@
+package syre.ms.microserviceproduct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroServiceProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
